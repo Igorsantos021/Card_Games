6b9fa296d6c8b79340ba48d55d9a128b7912e073
Igor Santos <santosigor868@gmail.com>
2024-03-25 21:35:47 -0300

    Up Finish

--- game/index.php
+++ game/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,17 +11,29 @@
         <div class="container">
           <h1 class="text-center">CARD GAME</h1>
             <div class="row">
-              <div class="col-lg-4 col-md-4 col-sm-12">
-                <div class="card" style="width: 18rem;">
-                   <img src="..." class="card-img-top" alt="...">
-                       <div class="card-body">
-                           <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                </div>
+              <?php
+              $arquivo = fopen("dados.csv","r");
+              while(!feof($arquivo)){
+                $linha = fgets($arquivo, 1000);
+                if($linha !=""){
+                    $dados = explode(";",$linha);
+                    ?>
+                    <div class="col-lg-4 col-md-4 col-sm-12">
+                      <div class="card" style="width: 18rem;">
+                        <img src="<?php echo $dados[2] ?>"class="card-img-top" alt="<?php echo $dados[0] ?>">
+                          <div class="card-body">
+                            <h5 class="card-title"><?php echo $dados[0] ?></h5>
+                            <p class="card-text"><?php echo $dados[1] ?></p>
+                          </div>
+                      </div>
 
-               </div>
+                    </div>
+                    <?php
+                }
+              }
+
+              ?>
+             
 
             </div>
 
